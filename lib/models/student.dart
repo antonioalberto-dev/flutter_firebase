27ec10b45490759f3c657e6ea17e6a914f11c63d
Antonio Alberto <antonioalbertodsb@gmail.com>
2023-01-20 11:23:09 -0300

    adicionando os metodos do crud na classe

--- lib/models/student.dart
+++ lib/models/student.dart
@@ -1,5 +1,9 @@
 class Student {
-  final name, email;
+  final id, name, email;
 
-  Student({required this.name, this.email});
+  Student({required this.id, required this.name, this.email});
+
+  Map<String, dynamic> addData() {
+    return {'name': name, 'email': email};
+  }
 }
